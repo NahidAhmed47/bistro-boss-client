82312ce70aaee077326b040c550c0c6192f43da9
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-05-23 15:19:32 +0600

    service section added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Category from '../Category/Category';
+import Service from '../Service/Service';
 
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <Category></Category>
+            <Service></Service>
         </div>
     );
 };
---
+++ src/pages/Home/Service/Service.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Service = () => {
+  return (
+    <div className="my-10 md:my-16 max-container bg-[url('https://i.ibb.co/7CN0Q0p/chef-service.jpg')] h-[500px] w-full flex justify-center items-center">
+      <div className="bg-white h-[60%] bg-opacity-75 w-[70%] text-center md:px-16  flex justify-center items-center">
+        <div className="space-y-4">
+          <h1 className="text-4xl font-bold tracking-[7px] font-mono">
+            Bistro Boss
+          </h1>
+          <p className="text-base text-slate-500">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut
+            voluptas eveniet numquam ipsam. Consequatur beatae harum architecto
+            repellat commodi rem quae veritatis. Quos expedita commodi animi
+            laborum qui odit accusamus. Consequatur beatae harum architecto
+            repellat commodi rem quae veritatis. Quos expedita commodi animi
+            laborum qui odit accusamus
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Service;
