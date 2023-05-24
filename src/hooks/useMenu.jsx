c512818c918130074d5fdbbd9dca5a11c5564355
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-05-24 17:54:00 +0600

    server link added

--- src/hooks/useMenu.jsx
+++ src/hooks/useMenu.jsx
@@ -5,7 +5,7 @@ const useMenu = ()=>{
     const [loading, setLoading] = useState(false)
     useEffect(()=>{
         setLoading(true)
-        fetch('/menu.json')
+        fetch('http://localhost:5000/menu')
         .then(res => res.json())
         .then(data => {
             setItemData(data)
