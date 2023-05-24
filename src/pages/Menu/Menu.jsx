fcea324695d34c100fcb76b1c8ed9da6c36a7a6f
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-05-24 15:05:46 +0600

    salads and soup section menu added

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -9,6 +9,9 @@ const Menu = () => {
   const [menu] = useMenu();
   const offered = menu.filter((item) => item.category === "offered");
   const desserts = menu.filter((item) => item.category === "dessert");
+  const pizza = menu.filter((item) => item.category === "pizza");
+  const salads = menu.filter((item) => item.category === "salad");
+  const soups = menu.filter((item) => item.category === "soup");
   return (
     <div>
       <PageBanner
@@ -30,12 +33,52 @@ const Menu = () => {
         <div className="w-full text-center mt-8">
           <button className="my-btn py-2">Order Your Favorite Food</button>
         </div>
+        {/* desserts section added */}
         <div>
           <SectionBanner title='Desserts'></SectionBanner>
           <div className="grid md:grid-cols-2 mt-10 gap-8">
           {desserts.map((item) => (
             <SingleItemMenu key={item._id} item={item}></SingleItemMenu>
           ))}
+        </div>
+        <div className="w-full text-center mt-8">
+          <button className="my-btn py-2">Order Your Favorite Food</button>
+        </div>
+        </div>
+        {/* pizza section added */}
+        <div>
+          <SectionBanner title='Pizza'></SectionBanner>
+          <div className="grid md:grid-cols-2 mt-10 gap-8">
+          {pizza.map((item) => (
+            <SingleItemMenu key={item._id} item={item}></SingleItemMenu>
+          ))}
+        </div>
+        <div className="w-full text-center mt-8">
+          <button className="my-btn py-2">Order Your Favorite Food</button>
+        </div>
+        </div>
+        {/* salads section added */}
+        <div>
+          <SectionBanner title='salads'></SectionBanner>
+          <div className="grid md:grid-cols-2 mt-10 gap-8">
+          {salads.map((item) => (
+            <SingleItemMenu key={item._id} item={item}></SingleItemMenu>
+          ))}
+        </div>
+        <div className="w-full text-center mt-8">
+          <button className="my-btn py-2">Order Your Favorite Food</button>
+        </div>
+        </div>
+        {/* soups section added */}
+        <div>
+          <SectionBanner title='soups'></SectionBanner>
+          <div className="grid md:grid-cols-2 mt-10 gap-8">
+          {soups.map((item) => (
+            <SingleItemMenu key={item._id} item={item}></SingleItemMenu>
+          ))}
+        </div>
+        <div className="w-full text-center mt-8">
+          <button className="my-btn py-2">Order Your Favorite Food</button>
         </div>
         </div>
       </div>
