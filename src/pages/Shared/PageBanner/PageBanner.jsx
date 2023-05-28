f5e6b251084141d88033767b23fd1542fb5d4985
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-05-28 16:47:07 +0600

    pages banner img position changed

--- src/pages/Shared/PageBanner/PageBanner.jsx
+++ src/pages/Shared/PageBanner/PageBanner.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const PageBanner = ({bannerTitle, subText, img}) => {
     return (
-        <div className={` bg-[url('${img}')] bg-no-repeat bg-cover h-[50vh] md:h-[80vh] w-full flex justify-center items-center `}>
-      <div className="bg-black h-[40%] bg-opacity-50 w-[50%] text-center md:px-16  flex justify-center items-center">
+        <div className={`relative h-[50vh] md:h-[80vh] w-full flex justify-center items-center overflow-hidden`}>
+      <img className='w-full h-auto' src={img} alt="" />
+      <div className="absolute bg-black h-[40%] bg-opacity-50 w-[50%] text-center md:px-16  flex justify-center items-center">
         <div className="space-y-4">
           <h1 className="text-5xl text-white font-bold tracking-[7px] font-mono uppercase">
             {bannerTitle}
