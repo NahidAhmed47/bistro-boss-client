3564a53b89ecb30d05222c8737631e288480f8a2
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-06-01 10:42:42 +0600

    error fixed

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -10,7 +10,7 @@ const useAxiosSecure = () => {
   const navigate = useNavigate(); 
 
   const axiosSecure = axios.create({
-    baseURL: 'http://localhost:5000', 
+    baseURL: 'https://bistro-boss-server-two.vercel.app/', 
   });
 
   useEffect(() => {
